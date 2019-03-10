3f267a29f2506405ad02a1e3d092e18137804eaa
James <jamesnicholasmartin@gmail.com>
2019-03-09 22:40:43 -0800

    Updated headers

--- Student.cs
+++ Student.cs
@@ -1,18 +1,32 @@
-﻿///////////////////////////////////////////////////////
-///TINFO 200 A, Winter 2019
-///UWTacoma
-///l6oop - C Sharp Programming Lab 6 - A Student Database App
-///This program creates a database for holding student information for a school
-///The user can look up a student record to view, edit the record and delete it
-///The user will be able to also save any record as well as quit the program
-///
-
-////////////////////////////////////////////////
+﻿//TINFO 200 A, Winter 2019
+//l6oop - C Sharp Programming Lab 6 - A Student Database App
+//This program creates a database for holding student information for a school
+//The user can look up a student record to view, edit the record and delete it
+//The user will be able to also save any record as well as quit the program
+//
 // Change History
-// Date     Name        Description
-//2/28/19   baint       Added create method as well as update method
-//3/5/19    baint       Added to create and update method to get them working 
-//3/7/19    baint       Added inheritance for Undergrad and Grad students
+// Date         Name                  Description
+// 02/19/2019   jmsnmrtn@uw.edu       Project creation and push to github
+//                                    Created Student class          
+// 02/25/2019   jmsnmrtn@uw.edu       Created Undergrad and Gradstudent classes  
+//                                    Added Gradstudent class
+//                                    Implimented find record
+//                                    Implimented create record
+//                                    Updated ToString methods
+//                                    Updated application header(Not complete)
+// 02/25/2019   jmsnmrtn@uw.edu       Updated many of the methods in the DatabaseApp.cs
+// 02/26/2019   jmsnmrtn@uw.edu       Updated UpdateRecord()
+//                                    Updated FindRecord()
+//                                    Finished ToString()
+// 02/28/2019   jmsnmrtn@uw.edu       Finished up DeleteRecord()
+//                                    Made changes to update record
+// 03/05/2019   jmsnmrtn@uw.edu       Finished Gradstudent Class
+//                                    Created DeleteRecord(int index) method
+// 03/09/2019   jmsnmrtn@uw.edu       Added Comments to code
+//                                    Finished all update methods
+//                                    Save record to disk on record creation
+//                                    Got project ready for submission
+//                                    Final push to github
 using System;
 using System.Threading;
 
